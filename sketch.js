2b0e7c9ebb93c2d9979d53ab166bbfa99d40a747
aschombert <54325483+aschombert@users.noreply.github.com>
2019-09-04 20:48:54 -0500

    mp1

---
+++ sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  // put setup code here
+  createCanvas(500,500);
+}
+
+function draw() {
+  // put drawing code here
+
+
+
+  if (mouseIsPressed) {
+    fill('green');
+    ellipse(250, 250, 50, 200);
+    rect(240,340, 5, 100);
+    rect(225,440, 20, 5);
+    rect(255,340, 5, 100);
+    rect(255,440, 20, 5);
+
+    fill('black');
+    ellipse(240,200,5,5);
+    ellipse(255,200,5,5);
+    ellipse(245,215,10,10);
+
+  }
+
+
+  else {
+    fill('green');
+    ellipse(250, 250, 50, 200);
+    rect(240,340, 5, 100);
+    rect(225,440, 20, 5);
+    rect(255,340, 5, 100);
+    rect(255,440, 20, 5);
+
+    fill('black');
+    ellipse(240,200,5,5);
+    ellipse(255,200,5,5);
+  }
+
+}
+
+function mouseReleased () {
+  console.log(mouseX + ',' + mouseY)
+}
