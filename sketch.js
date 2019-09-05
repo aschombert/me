e093370db79906c5ea874444d3d79f4a7bd6e149
aschombert <54325483+aschombert@users.noreply.github.com>
2019-09-04 21:34:05 -0500

    MP1- REAL!

--- sketch.js
+++ sketch.js
@@ -9,22 +9,30 @@ function draw() {
 
 
   if (mouseIsPressed) {
+    fill('red');
+    rect(225,250, 50, 100);
+
     fill('green');
     ellipse(250, 250, 50, 200);
     rect(240,340, 5, 100);
     rect(225,440, 20, 5);
     rect(255,340, 5, 100);
     rect(255,440, 20, 5);
+    rect(275,250, 50,5);
+    rect(175,250, 50,5);
 
     fill('black');
     ellipse(240,200,5,5);
     ellipse(255,200,5,5);
     ellipse(245,215,10,10);
 
+
+
   }
 
 
   else {
+
     fill('green');
     ellipse(250, 250, 50, 200);
     rect(240,340, 5, 100);
@@ -35,6 +43,7 @@ function draw() {
     fill('black');
     ellipse(240,200,5,5);
     ellipse(255,200,5,5);
+
   }
 
 }
